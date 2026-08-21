1b42b30edd35df7f81feed4ccb0b6c0b1e53f2f1
Devran Sever <hasan.buyuker@petour.com>
2026-08-21 14:56:16 +0300

    release: v5.5.3 - Multi-OS verified in-app updater test release

--- scripts/linux/build_deb.sh
+++ scripts/linux/build_deb.sh
@@ -4,7 +4,7 @@
 # ========================================================================
 set -e
 
-VERSION="5.5.2"
+VERSION="5.5.3"
 DEB_DIR="build_deb/postgresql-ddl-studio_${VERSION}_all"
 OUTPUT_DEB="PostgreSQL-DDL-Studio-${VERSION}-Linux.deb"
 
